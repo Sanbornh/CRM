f3d2589c5530aa4eb3afd69f964b9e13e05ff557
Sanborn <sanbornh@rogers.com>
2014-03-28 18:08:16 -0400

    Added quit function and add contact function to USER

--- user.rb
+++ user.rb
@@ -2,20 +2,30 @@ class User
 
 	def initialize
 		get_input
+
 	end
 
 	def main_menu
 		puts "\e[H\e[2J"
 		puts "---- Main Menu ----".center(25)
 		puts "1. Add new Contact"
+		puts "0. QUIT"
 	end
 
 	def get_input
 		main_menu
-		puts gets.chomp.to_i
-
+		perform(gets.chomp.to_i)
 	end
 
+	def perform(user_request)
+		if user_request == 1 then add_contact end
+		if user_request == 0 then end_program end
+	end
+
+	def end_program
+		puts "\e[H\e[2J"
+		puts "Goodbye"
+	end
 end
 
 user = User.new
